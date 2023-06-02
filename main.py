5c70517a9e332e2cac1c33ae2a6033a5d2378287
syedm1 <31075630+syedm1@users.noreply.github.com>
2023-06-02 11:10:59 +1000

    post

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request, jsonify
+import openai
 
 app = Flask(__name__)
 
+# Set up your OpenAI API credentials
+openai.api_key = "YOUR_API_KEY"
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+# Define a route for the API endpoint
+@app.route('/messages', methods=['POST'])
+def openai_request():
+    # Get the data from the request
+    data = request.json
 
+    # Extract the messages from the data
+    messages = data['messages']
+
+    return jsonify({'completion': messages})
+    # Prepare the conversation history for OpenAI API
+    conversation = []
+    for message in messages:
+        role = message['role']
+        content = message['content']
+        conversation.append({'role': role, 'content': content})
+
+    try:
+        # Make the request to OpenAI API
+        response = openai.Completion.create(
+            engine='davinci',
+            messages=conversation,
+            max_tokens=100,
+            temperature=0.6,
+            n=1,
+            stop=None
+        )
+
+        # Get the generated completion text from the response
+        completion_text = response.choices[0].message['content']
+
+        # Return the generated text as a response
+        return jsonify({'completion': completion_text})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run()
