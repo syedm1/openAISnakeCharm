efd6f664bd078ddc85bfe0be0943576224c7385f
syedm1 <31075630+syedm1@users.noreply.github.com>
2023-06-02 12:19:26 +1000

    Update main.py

--- main.py
+++ main.py
@@ -15,32 +15,19 @@ def openai_request():
     # Get the data from the request
     data = request.json
 
+    print(data)
     # Extract the messages from the data
-    messages = data['messages']
+    conversation = data['messages']
 
-    # Prepare the conversation history for OpenAI API
-    conversation = []
-    for message in messages:
-        role = message['role']
-        content = message['content']
-        conversation.append({'role': role, 'content': content})
+    print(conversation)
 
     try:
-        # Make the request to OpenAI API
-        response1 = openai.Completion.create(
-            model= OPEN_AI_ENGINE,
-            messages=conversation,
-        )
 
-        print(response1)
         response= openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                        {"role": "system", "content": "You are a helpful assistant."},
-                        {"role": "user", "content": "Who won the world series in 2020?"},
-                        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-                        {"role": "user", "content": "Where was it played?"}
-                    ]
+                model=OPEN_AI_ENGINE,
+                messages=conversation,
+                max_tokens=MAX_TOKENS,
+                temperature=0.9,
                 )
         # Return the generated text as a response
         return jsonify({'data': response})
