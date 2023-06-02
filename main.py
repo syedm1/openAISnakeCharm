6ca4cb50010ecc0cf3d15111db80897d775520ae
syedm1 <31075630+syedm1@users.noreply.github.com>
2023-06-02 14:33:09 +1000

    add /search method

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 import openai
 import os
+import json
+import json5
 
 app = Flask(__name__)
 
@@ -35,8 +37,45 @@ def openai_request():
 
         role = responseUltimate['role']
         content = responseUltimate['content']
+        contentJsonified = json5.loads(content)
 
-        return jsonify({'role': role, 'content': content})
+        return jsonify({'role': role, 'content': contentJsonified})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/search', methods=['POST'])
+def openai_request_search_only():
+    # Get the data from the request
+    data = request.json
+
+    print(data)
+    # Extract the messages from the data
+    conversation = data['messages']
+
+    print(conversation)
+
+    try:
+
+        response= openai.ChatCompletion.create(
+                model=OPEN_AI_ENGINE,
+                messages=conversation,
+                max_tokens=MAX_TOKENS,
+                temperature=0.9,
+                )
+        # Return the generated text as a response
+
+        responseUltimate = response['choices'][0]['message']
+
+        content = responseUltimate['content']
+        print(content)
+
+        search = json5.loads(content)
+
+        print("+++++++++++")
+        print(search)
+
+        return jsonify({"search": search})
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
